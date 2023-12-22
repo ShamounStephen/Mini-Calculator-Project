af93df643ffbbb67116e779f13a12d2c56ca3d99
ShamounStephen <151960454+ShamounStephen@users.noreply.github.com>
2023-12-23 01:43:54 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,28 @@
+"use strict";
+// var prompt = require('prompt-sync')();
+// let n1 = parseInt(prompt(`Please enter your first number:`));
+// let n2 = parseInt(prompt(`Please enter your first number:`));
+// let operator = prompt(`Please enter your operator (+, -, *, /):`);
+// if (operator == `+`){
+//     console.log(`the answer of ${n1} ${operator} ${n2} = ${n1+n2}`);
+// }
+// else if (operator == `-`){
+//     console.log(`the answer of ${n1} ${operator} ${n2} = ${n1-n2}`);
+// }
+// else if (operator == `*`){
+//     console.log(`the answer of ${n1} ${operator} ${n2} = ${n1*n2}`);
+// }
+// else if (operator == `/`){
+//     console.log(`the answer of ${n1} ${operator} ${n2} = ${n1/n2}`);
+// }
+// else {
+//     console.log(`Please select the correct operator`);
+// }
+// export{};
+Object.defineProperty(exports, "__esModule", { value: true });
+// let age : number = 22;
+// console.log(age.toFixed(10))
+let age;
+age = 22;
+age.toFixed(2); // narrowing 
+age = "22";
---
+++ calculator.ts
@@ -0,0 +1,23 @@
+
+var prompt = require('prompt-sync')();
+let n1 = parseInt(prompt(`Please enter your first number:`));
+let n2 = parseInt(prompt(`Please enter your first number:`));
+let operator = prompt(`Please enter your operator (+, -, *, /):`);
+if (operator == `+`){
+    console.log(`the answer of ${n1} ${operator} ${n2} = ${n1+n2}`);
+}
+else if (operator == `-`){
+    console.log(`the answer of ${n1} ${operator} ${n2} = ${n1-n2}`);
+}
+else if (operator == `*`){
+    console.log(`the answer of ${n1} ${operator} ${n2} = ${n1*n2}`);
+}
+else if (operator == `/`){
+    console.log(`the answer of ${n1} ${operator} ${n2} = ${n1/n2}`);
+}
+else {
+    console.log(`Please select the correct operator`);
+}
+export{};
+
+
